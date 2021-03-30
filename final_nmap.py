80ba408fe3cf2465a7fda0d96e7d2e1d178bf693
Dhruvi Shah <61619085+dhruvi897@users.noreply.github.com>
2021-03-30 11:03:21 +0530

    Update final_nmap.py

--- final_nmap.py
+++ final_nmap.py
@@ -8,19 +8,15 @@ print("<----------------------------------------------------->")
 ip_addr = input("Enter target IP address: ")
 print("The IP you entered is: ", ip_addr)
 type(ip_addr)
+print("Starting..")
 print("Nmap Version: ", scanner.nmap_version())
 scanner.scan(ip_addr, '1-65535', '-v -sS -sV -sC -A -O')
 print(scanner.scaninfo())
-"""print("Ip Status: ", scanner[ip_addr].state())
+print("Ip Status: ", scanner[ip_addr].state())
 print(scanner[ip_addr].all_protocols())
-print("Open Ports: ", scanner[ip_addr]['tcp'].keys())"""
+print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
 
 output=scanner.csv()
-
-"""with open("output_nmap.csv", "wb") as csv_file:
-	writer = csv.writer(csv_file, delimiter=';')
-	for line in output:
-        	writer.writerow(line)"""
         	
 l=list(output.split("\n"))
 
@@ -30,4 +26,5 @@ for i in range(len(l)):
 with open('output_nmap.csv','w') as f:
 	writer=csv.writer(f)
 	writer.writerows(l)
-	
+
+print("Check the output file, output_nmap.csv, for detailed scan results!")
